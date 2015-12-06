238ccd2150e97fc875a3421165bdf3684f6f8866
Taylor King <kingtb@student.cs.appstate.edu>
2015-12-06 12:20:38 +0000

    Consistent " things

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var express = require('express');
 var app = express();
 var awsregion = 'us-west-2';
 
-var host = "http://127.0.0.1:8081";
+var host = 'http://127.0.0.1:8081';
 
 var presto = require('presto-client');
-var client = new presto.Client({host:"127.0.0.1", "port":8081, catalog:'dynamo', schema: 'us_west_2'});
+var client = new presto.Client({host:'127.0.0.1', port:8081, catalog:'dynamo', schema: 'us_west_2'});
 var bodyParser = require('body-parser');
 var urlParser = bodyParser.urlencoded({extended: false});
 var keys = JSON.parse(fs.readFileSync('keys.json'));
@@ -17,18 +17,18 @@ app.get('/', urlParser, function(req, res) {
   console.log(req.query);
   if(keys.indexOf(req.query.key) > -1) { 
     res.send();
-    console.log("valid key");
+    console.log('valid key');
     client.execute(req.query.query, function (error, data, columns) {
-      console.log("Done");
+      console.log('Done');
 
       if(error) {
-        bot.sendMessage(req.query.chat, "Error with query: " + JSON.stringify(error), function(err, data){
+        bot.sendMessage(req.query.chat, 'Error with query: ' + JSON.stringify(error), function(err, data){
           if(err) {
               console.log(err);
             }
         });       
       } else {
-        bot.sendMessage(req.query.chat, "Result of query: " + JSON.stringify(data), function(err, data) {
+        bot.sendMessage(req.query.chat, 'Result of query: ' + JSON.stringify(data), function(err, data) {
           if(err) {
             console.log(err);
           }
@@ -37,7 +37,7 @@ app.get('/', urlParser, function(req, res) {
       }
     });
   } else {
-    console.log("bad key");
+    console.log('bad key');
     res.sendStatus(403);
   }
 });
